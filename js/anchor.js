e5c62c9bfcbdf71124f3143f91859f56db0058a2
Дмитрий Горелкин <87872078+Dmitry-Gorelkin@users.noreply.github.com>
2024-03-03 00:00:22 +0200

    add_scroll

---
+++ js/anchor.js
@@ -0,0 +1,12 @@
+$('a[href^="#"]').on('click', function (event) {
+  var anchor = $(this.getAttribute('href'));
+  if (anchor.length) {
+    event.preventDefault();
+    $('html, body').stop().animate(
+      {
+        scrollTop: anchor.offset().top,
+      },
+      500
+    );
+  }
+});
